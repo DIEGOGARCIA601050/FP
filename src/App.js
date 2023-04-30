41cc4290c790485d6839c088b38d328dfe1f7e10
DIEGOGARCIA601050 <ekrnal9@gmail.com>
2023-04-29 22:32:50 -0600

    MiguelHidalgo

--- src/App.js
+++ src/App.js
@@ -20,6 +20,18 @@ function App() {
           En noviembre de 1871, con la reelección del Presidente Juárez, se levantó en armas y proclamó el Plan de la Noria, sin lograr el éxito esperado, su movimiento fue derrotado. Posterior a ello se rebeló contra lerdo (Plan de Tuxtepec) con el cual se hizo del poder, para después forjar un estricto regimén que, a pesar de que modernizó el país, reprimió toda oposición, además de permitir que se maltratara a los mexicanos más pobres en las Haciendas minas y fabricas, algo que llegaria a su limte con las huelgas de Cananea(1909) y Rio Blanco (1907), por lo que Diaz se vió obligado a dimitir del cargo en 1911
         </p>
         <li>Miguel Hidalgo(1753-1811)<span className='cita'>"¡Viva la religión!, ¡viva nuestra madre santísima de Guadalupe!, ¡viva Fernando VII!, ¡viva la América y muera el mal Gobierno!"</span></li>
+        <p>Pues verás, Miguel Hidalgo fue un hombre muy importante en la historia de México. Él nació hace muchos años el 8 de mayo de 1753 en la hacienda de San Diego de Corralejo, en Pénjamo, Guanajuato, donde seguramente pasó tiempo cuidando animales, como vacas y caballos, y jugando con sus amigos.
+
+Cuando creció, se convirtió en sacerdote en 1803 y trabajó en la Parroquia de Dolores, Guanajuato. Ahí, ayudó a su comunidad enseñándoles a cultivar la tierra y a hacer objetos de cerámica y ladrillos. ¡Hasta les enseñó a criar abejas!
+
+Un día, Miguel Hidalgo decidió que México ya no debía ser gobernado por los españoles, así que se unió a una sociedad secreta formada en Valladolid que quería para gobernar el país como el rey Fernando VII y en su caso, obtener la independencia del país.
+. Entonces el 16 de septiembre de 1810, en Dolores, dio un grito muy fuerte llamando a la gente a unirse a la lucha por la independencia.
+
+Después de pelear muchas batallas, Miguel Hidalgo fue capturado y condenado a muerte. Fue fusilado en  Chihuahua el 30 de julio de 1811, los decapitaron y colgaron sus cabezas en la Alhóndiga de Granaditas de Guanajuato. pero su legado y su valentía se mantienen vivos hasta el día de hoy.</p>
+<figure>
+<img src=''></img>
+</figure>
+
       </ul>
       </main>
       <footer>
