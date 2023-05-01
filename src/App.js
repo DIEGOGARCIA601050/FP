a3f252cd9552103ec7e0bf7f322866c3dd1a716b
DIEGOGARCIA601050 <ekrnal9@gmail.com>
2023-04-30 20:22:18 -0600

    PDMyMHC

--- src/App.js
+++ src/App.js
@@ -12,25 +12,34 @@ function App() {
       <ul>
         <li>Benito Juarez(1806-1872) <span className='cita'>"Entre los individuos, como entre las naciones, el respeto al derecho ajeno es la paz"</span></li>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/79rBOsrcX6I" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-          <a href='https://youtu.be/79rBOsrcX6I'>Ver video en Youtube</a>
         <li>Porfirio Díaz(1830-1915)<span className='cita'>"Orden y Progreso"</span></li>
-        <p>Porfirio Díaz Empezó su carrera militar en 1854, al unirse al plan de Ayutla Debido a la persecusión que sufría por parte de Santa Anna, todo esto en un papel menor.
-          Posteriormente, la Guerra de Reforma de 1857 a 1860, fue Gobernador y Comandante Militar del Istmo en Oaxaca y logró alcanzar el grado de Coronel.
-          Además de participar en la mitica batalla del 5 de mayo, por ese y otros motivos, el presidente Juarez le otorgó el grado de General de División el 10 de octubre de 1863. en reconocimiento a sus actos.
-          En noviembre de 1871, con la reelección del Presidente Juárez, se levantó en armas y proclamó el Plan de la Noria, sin lograr el éxito esperado, su movimiento fue derrotado. Posterior a ello se rebeló contra lerdo (Plan de Tuxtepec) con el cual se hizo del poder, para después forjar un estricto regimén que, a pesar de que modernizó el país, reprimió toda oposición, además de permitir que se maltratara a los mexicanos más pobres en las Haciendas minas y fabricas, algo que llegaria a su limte con las huelgas de Cananea(1909) y Rio Blanco (1907), por lo que Diaz se vió obligado a dimitir del cargo en 1911
+        <p>Nació en la ciudad de Oaxaca un 15 de septiembre de 1830, ¡sí, el mismo día de la Independencia de México! Pero Porfirio no quería ser un simple espectador de la historia, así que ingresó al Instituto de Ciencias y Artes de Oaxaca para estudiar leyes, ¡quería ser un abogado muy top!
+
+Pero, ¡oh no! Los conflictos políticos del país no permitieron que Porfirio concluyera sus estudios, así que decidió luchar contra los conservadores en la Guerra de Reforma. Participó en la Batalla de Puebla durante la intervención francesa y logró huir cuando las fuerzas franco-mexicanas tomaron su ciudad natal.
+
+Pero no se rindió, ¡al contrario! Inició una serie de victorias militares y en 1876 triunfa la Revolución de Tuxtepec. ¡Y tachán! El 5 de mayo de 1877 toma posesión de la presidencia de México. Pero su reinado no terminaría ahí, ¡no señor! En 1884 asumió la presidencia nuevamente y no la abandonó hasta 1911, ¡ni más ni menos que 27 años en el poder! Pero entonces llegó Francisco I. Madero y convocó a la rebelión.
+
+Porfirio Díaz renunció a la presidencia en mayo de 1911 y abandonó el país. Y lamentablemente, amigos, ¡falleció en París, Francia, el 2 de julio de 1915! Aunque sus ideas y acciones siguen siendo objeto de controversia, su figura es parte importante de la historia de México y su legado sigue siendo recordado en la actualidad. ¡Gracias por acompañarme en este viaje a través del tiempo!
+          murió el 2 de julio de 1915 en la ciudad de París, Francia, a los 84 años. Pero lo curioso es que él siempre mantuvo la esperanza de regresar a su tierra natal, Oaxaca, y morir allí. ¡Imagínense eso! Pero bueno, ya saben que a veces las cosas no salen como uno quiere.
+
+Aunque Porfirio Díaz fue presidente de México por más de treinta años, su gobierno fue muy polémico por muchas cosas que hizo y dejó de hacer. Sin embargo, es interesante saber que, hasta el final de su régimen, él conservó su fortaleza y su carácter fuerte. ¡Eso sí que es admirable!
+
+En fin, después de su muerte, su esposa Carmelita mandó a construir una pequeña capilla en el cementerio Montparnasse, en París, donde se encuentran sus restos hasta el día de hoy. Y a pesar de todo lo que se ha dicho sobre él, su tumba sigue siendo visitada por turistas que le llevan flores y obsequios.
         </p>
         <li>Miguel Hidalgo(1753-1811)<span className='cita'>"¡Viva la religión!, ¡viva nuestra madre santísima de Guadalupe!, ¡viva Fernando VII!, ¡viva la América y muera el mal Gobierno!"</span></li>
         <p>Pues verás, Miguel Hidalgo fue un hombre muy importante en la historia de México. Él nació hace muchos años el 8 de mayo de 1753 en la hacienda de San Diego de Corralejo, en Pénjamo, Guanajuato, donde seguramente pasó tiempo cuidando animales, como vacas y caballos, y jugando con sus amigos.
 
-Cuando creció, se convirtió en sacerdote en 1803 y trabajó en la Parroquia de Dolores, Guanajuato. Ahí, ayudó a su comunidad enseñándoles a cultivar la tierra y a hacer objetos de cerámica y ladrillos. ¡Hasta les enseñó a criar abejas!
+Cuando creció, se convirtió en sacerdote en 1803 y trabajó en la Parroquia de Dolores, Guanajuato. Ahí, ayudó a su comunidad enseñándoles a cultivar la tierra y a hacer objetos de cerámica y ladrillos. ¡Hasta les enseñó a criar abejas!. Algunos dicen que era bastante liberal, pues su ejercito hacia un desastre en cuanto llegaban a una ciudad
 
 Un día, Miguel Hidalgo decidió que México ya no debía ser gobernado por los españoles, así que se unió a una sociedad secreta formada en Valladolid que quería para gobernar el país como el rey Fernando VII y en su caso, obtener la independencia del país.
 . Entonces el 16 de septiembre de 1810, en Dolores, dio un grito muy fuerte llamando a la gente a unirse a la lucha por la independencia.
 
 Después de pelear muchas batallas, Miguel Hidalgo fue capturado y condenado a muerte. Fue fusilado en  Chihuahua el 30 de julio de 1811, los decapitaron y colgaron sus cabezas en la Alhóndiga de Granaditas de Guanajuato. pero su legado y su valentía se mantienen vivos hasta el día de hoy.</p>
 <figure>
-<img src='./assets/Captura de pantalla 2023-04-30 152531.png'></img>
-<figcaption>Foto sacada de Dalle-2</figcaption>
+  <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DmkosRNN6N-RH2ypOFdbWAHaEK%26pid%3DApi&f=1&ipt=b733e46f444ddf34a4ccf9283a1214f7a381fd1633136e60c97744069d51d763&ipo=images'></img>
+  <figcaption>
+    Foto sacada de <a href='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DmkosRNN6N-RH2ypOFdbWAHaEK%26pid%3DApi&f=1&ipt=b733e46f444ddf34a4ccf9283a1214f7a381fd1633136e60c97744069d51d763&ipo=images'>https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DmkosRNN6N-RH2ypOFdbWAHaEK%26pid%3DApi&f=1&ipt=b733e46f444ddf34a4ccf9283a1214f7a381fd1633136e60c97744069d51d763&ipo=images</a>
+  </figcaption>
 </figure>
 
       </ul>
